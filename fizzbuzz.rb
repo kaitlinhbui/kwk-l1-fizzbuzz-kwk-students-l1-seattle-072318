a538926008963e9e0c87b2568f4bd491e8670be8
kaitlinhbui <kaitlinhb01@gmail.com>
2018-07-24 21:24:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@ int = gets.chomp
 def fizzbuzz(int)
   if #{int} % 3 == 0
     puts "Fizz"
-  elsif int % 5 == 0
+  elsif #{int} % 5 == 0
     puts "Buzz"
-  elsif int % 15 == 0
+  elsif #{int} % 15 == 0
     puts "FizzBuzz"
   else
     puts "Nil"

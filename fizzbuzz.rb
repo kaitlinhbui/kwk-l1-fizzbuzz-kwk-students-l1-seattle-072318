db6e11231616c2db25ee202607ea177da00fcc5a
kaitlinhbui <kaitlinhb01@gmail.com>
2018-07-24 21:23:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
-puts "Enter a number: "
-num = gets.chomp
+puts "Enter an integer: "
+int = gets.chomp
 
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 15 == 0
+    puts "FizzBuzz"
+  else
+    puts "Nil"
   end
 end
-
-fizzbuzz(5)
-fizzbuzz()

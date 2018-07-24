1e747d46145e54699eb2eaad8181b43eca2124f4
kaitlinhbui <kaitlinhb01@gmail.com>
2018-07-24 21:16:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,12 @@
+puts "Enter a number: "
+int = gets.chomp
+
 def fizzbuzz(int)
-  puts "Enter a number: "
-  int = gets.chomp
   if int % 3 == 0
-    "Fizz"
-  end
+    puts "Fizz"
 end
 
 fizzbuzz(3)
 
 fizzbuzz(5)
-
-fizzbuzz()
 
